61d6fc73654d34b87fbc39418bc3d3ef77bdb47f
adilalawi <adilalawi@hotmail.de>
2019-02-24 10:43:46 +0100

    Project Basic Stucture

---
+++ UnitTestIIQ_SCIM_API/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestIIQ_SCIM_API
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
