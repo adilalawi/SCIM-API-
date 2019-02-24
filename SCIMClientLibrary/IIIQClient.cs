b4f3c1480f0462fdc3b1df8052abbbdc5172948f
adilalawi <adilalawi@hotmail.de>
2019-02-24 11:04:20 +0100

    Add RestSharp Library

--- SCIMClientLibrary/IIIQClient.cs
+++ SCIMClientLibrary/IIIQClient.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,8 @@ namespace SCIMClientLibrary
 {
     interface IIIQClient
     {
+         RestRequest Request(string path, Method method);
+         IRestResponse Response(RestRequest request);
+         HttpStatusCode Login();
     }
 }
